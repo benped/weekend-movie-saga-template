09e6d0352506b0ff0fdf72619d11243dbd5dc18f
Ben Pedrick <bpedrick@gmail.com>
2022-03-05 08:53:47 -0600

    basic rendering done

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -8,11 +8,19 @@ function Details() {
     const movie = useSelector(store => store.detailMovie);
     console.log(movie);
 
+    const history = useHistory();
 
+    const backButton = () => {
+        console.log('Inside back button');
+        history.push("/");
+    }
 
     return (
         <>
-<h1> Hello World</h1>
+<h1> {movie.title}</h1>
+<img src={movie.poster}/>
+<p>{movie.description}</p>
+<button onClick={backButton}>Back to Movies</button>
         </>
     )
 }
