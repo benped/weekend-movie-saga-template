68720e9969a8c06d73c23a28d0de595229b063be
Ben Pedrick <bpedrick@gmail.com>
2022-03-05 08:21:37 -0600

    movie list /details click event written

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function MovieList() {
-
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
@@ -11,13 +13,18 @@ function MovieList() {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+    const detailHandler = (movieId) {
+        dispatch({type: 'SET_DETAIL_MOVIE', payload: movieId})
+        history.push('/details')
+    }
+
     return (
         <main>
             <h1>MovieList</h1>
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
+                        <div key={movie.id} onClick={detailHandler(movie.id)}>
                             <h3>{movie.title}</h3>
                             <img src={movie.poster} alt={movie.title}/>
                         </div>
