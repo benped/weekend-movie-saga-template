affc8826c5ebfd251795e05b08017eb5b15f745f
Ben Pedrick <bpedrick@gmail.com>
2022-03-05 12:42:17 -0600

    genres coming through on detail page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,34 +1,42 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
-
 function Details() {
-    const movie = useSelector(store => store.detailMovie);
-    const dispatch = useDispatch();
-    
-    useEffect(() => {
-        dispatch({ type: 'FETCH_GENRES',payload:movie });
-    }, []);
+  const movie = useSelector((store) => store.detailMovie);
+  const genres = useSelector((store) => store.detailGenre);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch({ type: "FETCH_GENRES", payload: movie });
+    console.log(genres);
+  }, []);
 
+  const history = useHistory();
 
-    const history = useHistory();
+  const backButton = () => {
+    console.log("Inside back button");
+    history.push("/");
+  };
 
-    const backButton = () => {
-        console.log('Inside back button');
-        history.push("/");
-    }
+  return (
+    <>
+      <h1> {movie.title}</h1>
+      <img src={movie.poster} />
+      <p>{movie.description}</p>
+      {/* MAP OF GENRES */}
+      {genres.length > 0 ? (
+        genres.map((genre, i) => 
+          <p key={i}>{genre.name}</p>
+        )
+      ) : (
+        <span>No length</span>
+      )}
 
-    return (
-        <>
-<h1> {movie.title}</h1>
-<img src={movie.poster}/>
-<p>{movie.description}</p>
-<button onClick={backButton}>Back to Movies</button>
-        </>
-    )
+      <button onClick={backButton}>Back to Movies</button>
+    </>
+  );
 }
 
-export default Details; 
+export default Details;
